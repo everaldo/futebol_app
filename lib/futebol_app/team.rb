076e1510e1420ee7ef25a463c7fdd46dbba03987
Everaldo Gomes <everaldo.gomes@gmail.com>
2013-07-24 14:30:58 -0300

    added print method and list of matches

--- lib/futebol_app/team.rb
+++ lib/futebol_app/team.rb
@@ -1,13 +1,23 @@
 module FutebolApp
   class Team
     attr_reader :players
-    attr_accessor :name
+    attr_accessor :name, :matches
 
 
     def initialize(options = {})
       @name = options.fetch :name, "undefined"
       @players = options.fetch :players, [] 
-
+      @matches = options.fetch :matches, []
     end
+
+    def add_match(match)
+      matches << match
+    end
+
+
+    def to_s
+      name
+    end
+
   end
 end
