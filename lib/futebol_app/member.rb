246025a9489ce25acc2199b4faa87bec0e0ea517
Everaldo Gomes <everaldo.gomes@gmail.com>
2013-07-28 19:54:16 -0300

    refactory member class. Created Statistics inner class to store information about games played

--- lib/futebol_app/member.rb
+++ lib/futebol_app/member.rb
@@ -1,11 +1,60 @@
 module FutebolApp
   class Member
+
+    class Statistics
+      attr_reader :member
+        attr_accessor :points, :games_played, :wins, :losses, :draws, :goals_scored, :goals_against, :results
+
+      def initialize(member)
+        @member         = member
+        @points         = 0
+        @games_played   = 0
+        @wins           = 0
+        @losses         = 0
+        @draws          = 0
+        @goals_scored   = 0
+        @goals_against  = 0
+        @results        = Hash.new 
+      end
+
+
+      def win!(result)
+        return if results.include?(result.id)
+        @results[result.id] = result
+        @points += 3
+        @games_played += 1
+        @wins += 1
+        @goals_scored += result.goals_for(member)
+        @goals_against += result.goals_for_opponent(member)
+      end
+      def loss!(result)
+        return if results.include?(result.id)
+        @results[result.id] = result
+        @games_played +=1
+        @losses +=1
+        @goals_scored += result.goals_for(member)
+        @goals_against += result.goals_for_opponent(member)
+      end
+
+      def draw!(result)
+        return if results.include?(result.id)
+        @results[result.id] = result
+        @points += 1
+        @games_played +=1
+        @draws +=1
+        @goals_scored += result.goals_for(member)
+        @goals_against += result.goals_for_opponent(member)
+      end
+    end
+    
+    
     extend Forwardable
     attr_reader :team, :id
     attr_accessor :points, :games_played, :wins, :losses, :draws,
                   :goals_scored, :goals_against, :results
 
     def_delegators :team, :name
+    def_delegators :@stats, :points, :games_played, :wins, :losses, :draws, :goals_scored, :goals_against, :results
 
     def initialize(options = {})
       @team         = options.fetch :team
@@ -17,43 +66,19 @@ module FutebolApp
     end
 
     def reset!
-      @points         = 0
-      @games_played   = 0
-      @wins           = 0
-      @losses         = 0
-      @draws          = 0
-      @goals_scored   = 0
-      @goals_against  = 0
-      @results        = Hash.new 
-    end
+      @stats = Statistics.new(self)
+     end
 
     def win!(result)
-      return if results.include?(result.id)
-      @results[result.id] = result
-      @points += 3
-      @games_played += 1
-      @wins += 1
-      @goals_scored += result.goals_for(self)
-      @goals_against += result.goals_for_opponent(self)
+      @stats.win!(result)
     end
 
     def loss!(result)
-      return if results.include?(result.id)
-      @results[result.id] = result
-      @games_played +=1
-      @losses +=1
-      @goals_scored += result.goals_for(self)
-      @goals_against += result.goals_for_opponent(self)
+      @stats.loss!(result)
     end
 
     def draw!(result)
-      return if results.include?(result.id)
-      @results[result.id] = result
-      @points += 1
-      @games_played +=1
-      @draws +=1
-      @goals_scored += result.goals_for(self)
-      @goals_against += result.goals_for_opponent(self)
+      @stats.draw!(result)
     end
 
     def goal_difference
